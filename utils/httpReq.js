a6c4aefa19cfd90908b4bc99396f7a31933a6bc3
mr.nobody <id4acc2014@protonmail.com>
2024-02-06 20:42:39 +0330

    build func postData

---
+++ utils/httpReq.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://fakestoreapi.com";
+
+const postData = async (path, data) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${path}`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    });
+    return await response.json();
+  } catch (error) {
+    alert("Something went wrong, please try again");
+  }
+};
+
+export { postData };
