f688ca9f9111f37e2462b3826e099acced2e7c33
mr.nobody <id4acc2014@protonmail.com>
2024-02-07 22:53:13 +0330

    build func filterHandler

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const dashboardButton = document.getElementById("dashboard");
 const mainContent = document.getElementById("products");
 const searchButton = document.querySelector("button");
 const inputBox = document.querySelector("input");
+const listItems = document.querySelectorAll("li");
 
 const showProducts = (products) => {
   mainContent.innerHTML = "";
@@ -62,5 +63,23 @@ const searchHandler = () => {
   showProducts(filteredProducts);
 };
 
+const filterHandler = (event) => {
+  const category = event.target.innerText.toLowerCase();
+
+  listItems.forEach((li) => {
+    if (li.innerText.toLowerCase() === category) {
+      li.className = "selected";
+    } else {
+      li.className = "";
+    }
+  });
+
+  if (category === "all") return showProducts(allProducts);
+  
+  const filteredProducts = allProducts.filter((product) => product.category.toLowerCase() === category);
+  showProducts(filteredProducts);
+};
+
 document.addEventListener("DOMContentLoaded", init);
 searchButton.addEventListener("click", searchHandler);
+listItems.forEach((li) => li.addEventListener("click", filterHandler));
